5e68d7dcc37804d125398dfdfb9d04bbb6708c85
Gangadhar Kadam <gangadhar.k@indictanstech.com>
2015-11-10 10:59:05 +0530

    addon

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -33,6 +33,21 @@ class User(Document):
 		if self.name not in STANDARD_USERS:
 			self.validate_email_type(self.email)
 
+		#Admin creation validation for one company
+		user = frappe.db.sql("""select u.name from tabUser u,tabUserRole r where r.parent=u.name and r.role='Admin' and 
+			u.company='%s'"""%(self.company),as_list=1)
+		if user and ("Admin" in [user_role.role for user_role in self.get("user_roles")]):
+			if user[0][0]!=self.name:
+				frappe.throw(_("Admin '{0}' is already created for company '{1}',you cannot create two admin users for one company !").format(user[0][0],self.company))
+
+		#Superadmin creation validation for company
+		superadmin = frappe.db.sql("""select u.name from tabUser u,tabUserRole r where r.parent=u.name and r.role='Administrator'""",as_list=1)
+		if superadmin and ("Administrator" in [user_role.role for user_role in self.get("user_roles")]):
+			if superadmin[0][0]!=self.name:
+				frappe.throw(_("SuperAdmin is already created,you cannot create new superadmin !").format(superadmin[0][0],self.company))
+
+
+		#Validation for user creation companywise
 		role = frappe.db.sql("select  u.name from tabUser u , tabUserRole r where r.parent=u.name and r.role='Admin' and u.name='%s'" %(frappe.session.user))
 
 		if self.get("__islocal") and role :
